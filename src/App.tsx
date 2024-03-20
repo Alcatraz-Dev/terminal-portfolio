5b29b5b70642e451713eb1d13c690ef0ba8f130c
Haythem Dhahri <alcatraz3019@gmail.com>
2024-03-20 20:47:47 +0100

    change the metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import BackgroundImage from "./components/BackgroundImage";
 import TerminalBox from "./components/TerminalBox";
 
@@ -7,11 +7,11 @@ export default function App() {
 		<>
 			<div className="h-screen w-full flex justify-center items-center  font-josefin">
 				<div className="w-full">
-					<div className="block sm:hidden mb-10 bg-yellow-200 py-3  text-center text-xl text-gray-700">
-						<p>For better experience please try this with pc!</p>
+					<div className="block sm:hidden mb-10 bg-yellow-300 py-3  text-center text-xl text-gray-700">
+						<p>For better experience please try this with pc !</p>
 						<p>This is not design for mobile view.</p>
-						<a href="https://chensokheng.me" className="block p-2 underline">
-							Visit this instead
+						<a href="https://haythem-dhahri.vercel.app/" className="block p-2 underline">
+							Visit this instead 
 						</a>
 					</div>
 					<TerminalBox />
