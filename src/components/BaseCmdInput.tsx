f35f91f50633afd1718ab5bd63b7a844fab37206
Haythem Dhahri <76733338+Alcatraz-Dev@users.noreply.github.com>
2024-03-21 00:50:36 +0100

    Update BaseCmdInput UI

--- src/components/BaseCmdInput.tsx
+++ src/components/BaseCmdInput.tsx
@@ -21,7 +21,7 @@ export default function BaseCmdInput({
           <span> - </span> <GiOpenFolder />
         </div>
         <div
-          className=" w-5 h-5 bg-gray-500 z-20"
+          className=" w-5 h-5 bg-gray-500 z-40"
           style={{
             clipPath: "polygon(0 0, 100% 50%, 0 100%)",
           }}
